92f057fb89fa9936af1482b07c44b8ff58897687
MinhNoob <minhtranconglis@gmail.com>
2023-03-02 11:53:22 +0700

    dev

--- controller/attribute.controller.js
+++ controller/attribute.controller.js
@@ -1,9 +1,10 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const type = ["Text", "Select", "Multiselect", "Textarea"];
+
 const createAttributeGroup = async (req, res) => {
   try {
-    console.log(req.body.group_name);
     const attributeGroup = await prisma.atrributeGroup.create({
       data: {
         group_name: req.body.group_name,
@@ -11,6 +12,31 @@ const createAttributeGroup = async (req, res) => {
     });
     res.status(200).json({
       result: attributeGroup.group_name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error,
+    });
+  }
+};
+
+const createAttribute = async (req, res) => {
+  try {
+    if (!type.includes(req.body.type)) {
+      res.status(400).json({
+        res: "incorrect type",
+      });
+    }
+    const atrribute = await prisma.atrribute.create({
+      data: {
+        attribute_code: req.body.attribute_code,
+        attribute_name: req.body.attribute_name,
+        type: req.body.type,
+        is_required: req.body.is_required,
+        display_on_frontend: req.body.display_on_frontend,
+        sort_order: req.body.sort_order,
+        is_filterable: req.body.is_filterable,
+      },
     });
   } catch (error) {
     res.status(500).json({
