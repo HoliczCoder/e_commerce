a70169785c124ccc57563634def224a27ccd9ba0
MinhNoob <minhtranconglis@gmail.com>
2023-03-10 16:54:20 +0700

    create product in process

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,9 +1,9 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const createProduct = (req, res) => {
+const createProduct = async (req, res) => {
   try {
-    const product = prisma.product.create({
+    const product = await prisma.product.create({
       data: {
         ...(req.body.variant_group_id && {
           variant_group_id: req.body.variant_group_id,
@@ -22,6 +22,84 @@ const createProduct = (req, res) => {
         status: req.body.status,
       },
     });
+
+    // save ProductAttributeValueIndex
+    // const attributes = await prisma.product.findMany({});
+    const attributes = req.body.attributes;
+    for (let i = 0; i < attributes.length; i += 1) {
+      const attribute = attributes[i];
+      if (attribute.value) {
+        const attr = await prisma.atrribute.findUnique({
+          where: {
+            attribute_code: attribute.attribute_code,
+          },
+        });
+        if (!attr) {
+          return;
+        }
+        if (attr.type === "textarea" || attr.type === "text") {
+          const flag = await prisma.productAttributeValueIndex.findUnique({
+            where: {
+              product_id: product.product_id,
+            },
+          });
+          if (flag) {
+            await prisma.productAttributeValueIndex.update({
+              where: {
+                product_id: product.product_id,
+              },
+              data: {
+                option_text: attribute.value.trim(),
+              },
+            });
+          } else {
+            await prisma.productAttributeValueIndex.create({
+              data: {
+                product_id: product.product_id,
+                attribute_id: attr.attribute_id,
+                option_text: attribute.value.trim(),
+              },
+            });
+          }
+        } else if (attr.type === "multiselect") {
+          await Promise.all(
+            attribute.value.map(() => {
+              (async () => {
+                const option = await prisma.attributeOption.findUnique({
+                  where: {
+                    attribute_option_id: attribute.value,
+                  },
+                });
+                if (!option) {
+                  res.status(404).json({
+                    res: "this attribute not exist",
+                  });
+                  return;
+                }
+                await prisma.productAttributeValueIndex.create({
+                  data: {
+                    product_id: product.product_id,
+                    attribute_id: attr.attribute_id,
+                    option_id: option.attribute_option_id,
+                    option_text: option.option_text,
+                  },
+                });
+              })();
+            })
+          );
+          //
+        } else if (attr.type === "select") {
+          const option = await prisma.attributeOption.findUnique({
+            where: {
+              attribute_option_id: attribute.value,
+            },
+          });
+          if (!option) {
+            ///////////////////////////// so tired
+          }
+        }
+      }
+    }
 
     res.status(200).res({ res: product });
   } catch (error) {
