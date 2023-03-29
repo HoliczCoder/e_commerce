5ab208625640600e9841290e0963979609500c16
MinhNoob <minhtranconglis@gmail.com>
2023-03-29 10:01:16 +0700

    read file .schema successfull

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,72 +1,21 @@
 const { ApolloServer } = require("@apollo/server")
 const ProductResolver = require("./product/variant/Product.resolver")
+const {join} = require('node:path')
+const { loadFilesSync } = require('@graphql-tools/load-files');
+const { mergeTypeDefs } = require('@graphql-tools/merge');
+
+const typeSources = [
+  join(__dirname, './**/*.graphql')
+];
+
+const typeDefs = mergeTypeDefs(
+  typeSources.map((source) => loadFilesSync(source))
+);
+
 
 const resolvers = {
   ...ProductResolver,
 };
-
-const typeDefs = `#graphql
-
-  type Product {
-  product_id: Int!
-  uuid: String!
-  name: String!
-  status: Int!
-  sku: String!
-  weight: Float!
-  tax_class: Int
-  description: String
-  url_key: String
-  meta_title: String
-  meta_description: String
-  meta_keywords: String
-  variant_group_id: ID
-  visibility: Int
-  group_id: ID
-  #   custom field
-  categories: [Category]
-  url: String
-  editUrl: String
-  updateApi: String!
-  deleteApi: String!
-}
-
-type Query {
-  product(id: ID): Product
-}
-
-type Category {
-  # exist field
-  category_id: ID!
-  uuid: String!
-  # join CategoryDescription table, is prisma allowed to join?
-  name: String!
-  status: Int!
-  include_in_nav: Int!
-  description: String
-  url_key: String
-  meta_title: String
-  meta_description: String
-  meta_keywords: String
-  #   is this exist?
-  #   image: CategoryImage
-  # products(filters: [FilterInput]): ProductCollection
-  #   custom field
-  # url: String
-  # editUrl: String
-  # updateApi: String!
-  # deleteApi: String!
-  # availableAttributes: [FilterAttribute]
-  # priceRange: PriceRange
-}
-
-# type ProductCollection {
-#   items: [Product]
-#   currentPage: Int!
-#   total: Int!
-#   currentFilters: [Filter]
-# }
-`;
 
 //
 const server = new ApolloServer({
